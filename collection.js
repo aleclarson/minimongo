8509281421d0a8b931c0b6695ca6771cb98342f5
Gabriele Petronella <gabriele@buildo.io>
2015-10-11 16:32:07 +0200

    Export collection and index properly

--- collection.js
+++ collection.js
@@ -1,2 +1,12 @@
-
 module.exports = require('./src/collection');
+
+require('./src/cursor');
+require('./src/wrapTransform');
+require('./src/selector');
+require('./src/sort');
+require('./src/projection');
+require('./src/modify');
+require('./src/diff');
+require('./src/idMap');
+require('./src/observe');
+require('./src/objectId');
--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 
 module.exports = require('./minimongo');
 
-require('./collection');
-require('./cursor');
-require('./wrapTransform');
-require('./selector');
-require('./sort');
-require('./projection');
-require('./modify');
-require('./diff');
-require('./idMap');
-require('./observe');
-require('./objectId');
+require('./src/collection');
+require('./src/cursor');
+require('./src/wrapTransform');
+require('./src/selector');
+require('./src/sort');
+require('./src/projection');
+require('./src/modify');
+require('./src/diff');
+require('./src/idMap');
+require('./src/observe');
+require('./src/objectId');
